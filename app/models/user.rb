284d0b4f444df28ecd863c8e28fc9e8c8147e842
kitpao <kitpao91@gmail.com>
2020-05-15 14:47:12 -0500

    :sparkles: Add controllers for user and a new view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true
-  validates :email , presence: true
+  validates :email, presence: true
   validates :password, presence: true, messsage: 'Think more, be original, use brain'
 end
