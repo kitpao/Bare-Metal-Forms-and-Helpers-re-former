de7215423e1c75b2d25257c86cfbcfff47212484
Eduardo <jecruxiz@gmail.com>
2020-05-15 15:45:42 -0500

    :sparkles: Introduce form templates, and some logic to controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
 
   def new
-
+    @user = User.new
   end
 
   def create
+     @user = User.new(username: params[:user][:username], password: params[:user][:password], email: params[:user][:email])
 
+     if @user.save
+      redirect_to new_users_path
+     else 
+      render template: 'users/new'
+     end
   end
 end
