db1b085230bd749181317386c2669906beea556d
Eduardo <jecruxiz@gmail.com>
2020-05-18 17:04:40 -0500

    :recycle: Refactoring code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_users_path
+      redirect_to new_user_path
     else
       render :new
     end
